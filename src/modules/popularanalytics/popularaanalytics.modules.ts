1a248b1f2a1a7c2d3c1ab2b6c68ebe1e6efda135
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-10-29 13:45:12 +0530

    Addefd

--- src/modules/popularanalytics/popularaanalytics.modules.ts
+++ src/modules/popularanalytics/popularaanalytics.modules.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service'; // Adjust the path as necessary
-import { MostlySearchedService } from './popularanalytics.services'; // Adjust the path as necessary
-import { MostlySearchedController } from './popularanalytics.controller'; // Adjust the path as necessary
+import { MostlySearchedService, MostlyViewedService } from './popularanalytics.services'; // Adjust the path as necessary
+import { MostlySearchedController, MostlyViewedController } from './popularanalytics.controller'; // Adjust the path as necessary
 
 @Module({
   imports: [],
-  controllers: [MostlySearchedController],
-  providers: [MostlySearchedService, PrismaService],
-  exports: [MostlySearchedService], // Exporting the service if needed in other modules
+  controllers: [MostlySearchedController, MostlyViewedController],
+  providers: [MostlySearchedService, MostlyViewedService, PrismaService],
+  exports: [MostlySearchedService, MostlyViewedService], // Exporting the service if needed in other modules
 })
 export class PopularAnalyticsModule {}
