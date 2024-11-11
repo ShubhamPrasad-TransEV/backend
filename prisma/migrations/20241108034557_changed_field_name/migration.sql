856d3937eac0164de1b3883f6043224fb551b209
SujataR1 <transmogrify06@outlook.com>
2024-11-11 15:13:07 +0530

    initial

--- prisma/migrations/20241108034557_changed_field_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `default` on the `address` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `address` DROP COLUMN `default`,
-    ADD COLUMN `defaultAddress` BOOLEAN NOT NULL DEFAULT false;
