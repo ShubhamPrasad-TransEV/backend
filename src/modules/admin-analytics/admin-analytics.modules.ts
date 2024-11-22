b5238ad1e970a17381eabcefc72ac805b0bc1f63
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-22 12:04:24 +0530

    U

--- src/modules/admin-analytics/admin-analytics.modules.ts
+++ src/modules/admin-analytics/admin-analytics.modules.ts
@@ -2,12 +2,12 @@
 
 import { Module } from '@nestjs/common';
 import { AnalyticsService } from './admin-analytics.service';
-import { AnalyticsController } from './admin-analytics.controller';
+import { AdminAnalyticsController } from './admin-analytics.controller';
 import { PrismaService } from '../../prisma/prisma.service'; // Assuming PrismaService is already set up
 
 @Module({
   imports: [],
-  controllers: [AnalyticsController],
+  controllers: [AdminAnalyticsController],
   providers: [AnalyticsService, PrismaService],
 })
 export class AdminAnalyticsModule {}
